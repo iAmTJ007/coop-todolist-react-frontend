e26f0efdb3155890d85275319cb3504e0a5bbb98
Tathagya <eveninghappy11@gmail.com>
2026-03-09 03:16:34 +0530

    added few things

---
+++ src/Elements/AddTaskModal.js
@@ -0,0 +1,38 @@
+import API from "../api/axios";
+import { useState } from "react";
+import Button from "./Button";
+
+function AddTaskModal({setShowModal,fetchTodos}){
+    const [form,setForm]=useState({title:"",description:"",priority:"",dueDate:""});
+
+    const handleAddTask=async(e)=>{
+        e.preventDefault();
+        try {
+            const res=await API.post("/todo/createTask",form);
+            alert("task created successfully");
+            setShowModal(false);
+            fetchTodos();
+        } catch (error) {
+            alert("failed to create task");
+        }
+    }
+    const handleCancel=()=>{
+        setShowModal(false);
+    }
+    
+    return(
+        <div className="addtaskmodal">
+            <form onSubmit={handleAddTask}>
+                <input placeholder="title" onChange={(e)=>setForm({...form,title:e.target.value})}/>
+                <input placeholder="description" onChange={(e)=>setForm({...form,description:e.target.value})}/>
+                <input placeholder="priority" onChange={(e)=>setForm({...form,priority:e.target.value})}/>
+                <input placeholder="duedate" type="date" onChange={(e)=>setForm({...form,dueDate:e.target.value})}/>
+                <Button text={"Add Task"}/>
+            </form>
+            <Button text={"Cancel"} onClick={handleCancel}/>
+            
+        </div>
+    )
+
+}
+export default AddTaskModal;
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,15 +3,21 @@ import API from "../api/axios";
 import Navbar from "../Elements/Navbar";
 import Todo from "../Elements/Todo";
 import DatePickerField from "../Elements/DatePickerField";
+import AddTaskModal from "../Elements/AddTaskModal";
+import '../index.css';
 function Home(){
     const [todos,setTodos]=useState([]);
     const [loading,setLoading]=useState(false);
-    const [date,setDate]=useState(new Date()); //yyyy-mm-dd
-    //fetch todos list
+    const [date,setDate]=useState(new Date());
+    const [showModal,setShowModal]=useState(false);
+
+    const selectedDate = date.toLocaleDateString("en-CA");
+
     const fetchTodos=async()=>{
         try {
             setLoading(true);
             const res=await API.get("/todo/getTask");
+            console.log(res);
             setTodos(res.data);
         } catch (error) {
             console.error("failed to fetch todos");
@@ -19,20 +25,46 @@ function Home(){
             setLoading(false);
         }
     }
+
     useEffect(()=>{
         fetchTodos();
     },[]);
+
     return(
-        <div className="home">
-            <Navbar/>  
-            <DatePickerField value={date} onChange={(date)=>setDate(date)}/>
-            <ul>
-                {todos.map((todo)=>{
-                    return(
-                        todo.createDate===date.toISOString().split("T")[0]?<Todo todo={todo}/>:null
+        <div className="screen">
+            <Navbar/>
+
+            <div className="home">
+
+                <DatePickerField
+                    value={date}
+                    onChange={(date)=>setDate(date)}
+                />
+
+                {
+                    loading ? (
+                        <h4>Loading tasks for the day</h4>
+                    ) :(
+                        <ul>
+                            {todos.map((todo)=>
+                                todo.createDate === selectedDate
+                                    ? <Todo key={todo.id} todo={todo}/>
+                                    : "null"
+                            )}
+                        </ul>
                     )
-                })}
-            </ul>   
+                }
+                {
+                    showModal ? (
+                        <AddTaskModal
+                            setShowModal={setShowModal}
+                            fetchTodos={fetchTodos}
+                        />
+                    ) : <button onClick={()=>setShowModal(true)}>
+                            Add Task
+                        </button>
+                }
+            </div>
         </div>
     )
 }
